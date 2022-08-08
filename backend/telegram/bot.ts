d86028a32a91ab7c0282155737ca866242aea3d9
aryarte <xinjiangcrosin@gmail.com>
2022-08-08 17:43:29 +0800

    create bot telegram

--- backend/telegram/bot.ts
+++ backend/telegram/bot.ts
@@ -3,7 +3,46 @@ import {botToken} from "../config"
 const bot = new Telegraf(botToken)
 
 bot.start((ctx: Context) => {
-  ctx.reply("Silahkan Pilih")
+  // FIRST REPLY
+  ctx.reply("Silahkan Pilih", {
+    reply_markup : {
+      inline_keyboard: [
+        [
+          {
+            text: "Tiket Regular",
+            callback_data: "tiket_regular"
+          }, {
+            text: "Lapor Langsung",
+            callback_data: "lapor_langsung"
+          }
+        ],
+        [
+          {
+            text: "Tutup ODP",
+            callback_data: "tutup_od p"
+          },{
+            text: "Proman",
+            callback_data: "proman"
+          }
+        ],
+        [
+          {
+            text: "Tiket SQM",
+            callback_data: "tiket_sqm"
+          },{
+            text: "Valins",
+            callback_data: "valins"
+          }
+        ],[
+          {
+            text: "Unspect",
+            callback_data: "unspect"
+          }
+        ]
+      ]
+    }
+  })
+  // END REPLY
 })
 
 export default bot
