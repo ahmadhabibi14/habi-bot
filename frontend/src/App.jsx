3d199d294d49d67ecaf5e912241fd49cda4f4967
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2022-08-11 13:46:10 +0800

    Tampilin data tabel dengan Axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,48 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home.jsx";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+// import Home from "./components/Home.jsx";
 import Dashboard from "./components/Dashboard.jsx";
-import TL_Board from "./components/TlBoard.jsx";
+import TlBoard from "./components/TlBoard.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <Home/>
-      <Routes>
-        <Route path="/dashboard" element={ <Dashboard/> } />
-        <Route path="/tl-board" element={ <TL_Board/> } />
-      </Routes>
+      {/* Container untuk Dashboard nya */}
+      <section className="flex flex-row space-x-4 p-6 text-slate-900">
+        {/* Navigasi Bar */}
+        <nav className="flex flex-col space-y-3 mt-6">
+          <NavLink
+            exact
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-emerald-500 py-2 px-6 text-slate-50"
+                : "bg-slate-300 py-2 px-6"
+            }
+          >
+            DASHBOARD
+          </NavLink>
+          <NavLink
+            to="/tl-board"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-emerald-500 py-2 px-6 text-slate-50"
+                : "bg-slate-300 py-2 px-6"
+            }
+          >
+            TL BOARD
+          </NavLink>
+        </nav>
+
+        {/* Dashboard */}
+        <main className="w-full border-2 border-slate-900 mt-2 p-4 min-h-max">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/tl-board" element={<TlBoard />} />
+          </Routes>
+        </main>
+      </section>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,11 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function dashboard() {
-	return (
-		<div>
-			dashboard
-		</div>
-	)
+function Dashboard() {
+  // Hooks
+  const [data, setData] = useState([]);
+
+  // AMBIL JSON dari external
+  const fetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((json) => {
+      // console.log(json.data);
+      setData(json.data);
+    });
+  };
+
+  // Ini biar TABEL nya jalan otomatis saat web di load
+  useEffect(() => {
+    let ignore = false;
+
+    if (!ignore) fetchData();
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col space-y-4">
+      {/* Input Tanggal */}
+      <div className="flex flex-row space-x-4 justify-end">
+        <select
+          name="Dates"
+          id="SelectDates"
+          className="py-2 px-4 bg-inherit border-2 border-slate-900 rounded-none"
+        >
+          <option value="">JAN 21</option>
+          <option value="FEB 18">FEB 18</option>
+          <option value="JUL 05">JUL 05</option>
+        </select>
+
+        <button className="py-2 px-4 border-2 border-slate-900">Filter</button>
+      </div>
+      {/* END */}
+
+      {/* Title */}
+      <h2 className="text-2xl font-bold ml-24">TABEL</h2>
+
+      {/* Tempat Tabel data-data dari teknisi nya */}
+      <div className="w-full">
+        <table className="w-full border-2 border-slate-900 mx-auto">
+          {/* Ket. Tabel */}
+          <thead>
+            <tr>
+              {[["Name"], ["Email"], ["Address"], ["Company"]].map(
+                ([isiData]) => (
+                  <td className="bg-slate-900 text-slate-50">{isiData}</td>
+                )
+              )}
+            </tr>
+          </thead>
+          {/* AKhir head tabel */}
+
+          {/* Isi Tabel */}
+          <tbody>
+            {data.map((user) => {
+              return (
+                <tr>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.address.street}</td>
+                  <td>{user.company.name}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+          {/* Akhir Isi Tabel */}
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default dashboard;
+export default Dashboard;
