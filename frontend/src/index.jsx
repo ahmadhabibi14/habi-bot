0ab4a96e621c1c1df1039717016758852dfd461f
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2022-08-12 11:36:11 +0800

    Login

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+// import Login from "./components/login-reg/login.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
