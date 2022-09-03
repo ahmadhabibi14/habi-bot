62b33e3e5de498bd889f0d5e88d1fa57464717ec
aryarte <xinjiangcrosin@gmail.com>
2022-09-03 20:22:53 +0800

    finishing

--- Application/config.js
+++ Application/config.js
@@ -3,4 +3,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbUrl = exports.botToken = void 0;
 exports.botToken = "5450247144:AAFEzzR0jOKuNxhW5BGz3V_5Mq9ijV1sl0U";
-exports.dbUrl = "mongodb+srv://ary:komang@cluster0.91nmd.mongodb.net/?retryWrites=true&w=majority";
+exports.dbUrl = "mongodb://127.0.0.1:27017"
+//exports.dbUrl = "mongodb+srv://***:******@cluster0.91nmd.mongodb.net/?retryWrites=true&w=majority";
